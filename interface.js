e4bc7d311f9fb360b3a5c69ba2263b2b477a7194
Ensar <bote.ensargunaydin@gmail.com>
2019-12-31 12:00:04 +0300

    no message

--- interface.js
+++ interface.js
@@ -1,4 +1,15 @@
 function save(product) {
     console.log(product.name + " kaydedildi");
 }
+function save2(product2) {
+    console.log(product2.name + " kaydedildi");
+}
+var Product2 = /** @class */ (function () {
+    function Product2() {
+    }
+    return Product2;
+}());
 save({ id: 1, name: "Laptop", unitPrice: 4 });
+var mouse = new Product2();
+mouse.name = "Logitech";
+save2(mouse);
--- interface.ts
+++ interface.ts
@@ -1,3 +1,8 @@
+/**
+ * Class - Interface farkı
+ * Eğer tüm parametreleri vermemiz gerekiyorsa Interface, gerekmiyorsa Class kullanılabilir
+ */
+
 interface Product {
     id:number;
     name:string;
@@ -8,4 +13,23 @@ function save(product:Product) {
     console.log(product.name + " kaydedildi")
 }
 
+
+function save2(product2:Product2) {
+    console.log(product2.name + " kaydedildi")
+}
+
+
+
+class Product2 {
+    id:number;
+    name:string;
+    unitPrice:number;
+}
+
 save({id:1, name:"Laptop", unitPrice:4})
+
+
+let mouse = new Product2()
+mouse.name = "Logitech"
+
+save2(mouse)
